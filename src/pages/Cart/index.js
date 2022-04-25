c0f9ca33862d0e56ff990d4d775aaa66d1e973f0
Marcelo Cristian <marcelo.cristian.969@gmail.com>
2022-04-25 05:45:28 -0300

    Redux finished

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -45,6 +45,18 @@ export default function Cart() {
     return formatValue(cartAmount);
   }, [products]);
 
+  function increment(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount + 1));
+  }
+
+  function decrement(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount - 1));
+  }
+
+  function removeFromCart(id) {
+    dispatch(CartActions.removeFromCart(id));
+  }
+
   return (
     <Container>
       <ProductContainer>
@@ -74,10 +86,13 @@ export default function Cart() {
                 </ProductPriceContainer>
               </ProductTitleContainer>
               <ActionContainer>
-                <ActionButton onPress={() => {}}>
+                <ActionButton onPress={() => increment()}>
                   <FeatherIcon name="plus" color="#e83f5b" size={16} />
                 </ActionButton>
-                <ActionButton onPress={() => {}}>
+                <ActionButton
+                  onPress={() =>
+                    item.amount > 1 ? decrement(item) : removeFromCart(item.id)
+                  }>
                   <FeatherIcon name="minus" color="#e83f5b" size={16} />
                 </ActionButton>
               </ActionContainer>
